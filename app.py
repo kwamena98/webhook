9322f2878eb2ff335e387be6dfe55b4fbb018f19
Dadson Derrick <63855156+kwamena98@users.noreply.github.com>
2022-11-29 16:22:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ import json
 
 import os
 import openai
-
-openai.api_key ="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"
-
-
-
-
-
-
-
-
+# os.environ["OPENAI_API_KEY"] = "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"
+# openai.api_key =os.getenv("OPENAI_API_KEY")
 
 
 
@@ -23,19 +15,19 @@ app = Flask(__name__)
 @app.route('/my_webhook', methods=['POST'])
 def return_response():
     print(request.json)
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=request.json,
-    temperature=0.9,
-    max_tokens=150,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    )
+    # response = openai.Completion.create(
+    # model="text-davinci-003",
+    # prompt=request.json,
+    # temperature=0.9,
+    # max_tokens=150,
+    # top_p=1,
+    # frequency_penalty=0,
+    # presence_penalty=0.6,
+    # )
 
-    data_=response["choices"][0]["text"]
-    print("from:",data_)
+    # data_=response["choices"][0]["text"]
+    # print("from:",data_)
     ## Do something with the request.json data.
-    return (data_)
+    return ("HI")
 if __name__ == "__main__":
     app.run()
